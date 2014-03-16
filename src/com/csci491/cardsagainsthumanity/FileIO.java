28b79fdaed72271d56d39585e423f06ee4ba492c
Andrew Meissner <andrew.meissner@outlook.com>
2014-03-15 22:51:17 -0600

    modified the FileIO class

--- src/com/csci491/cardsagainsthumanity/FileIO.java
+++ src/com/csci491/cardsagainsthumanity/FileIO.java
@@ -1,27 +1,31 @@
 package com.csci491.cardsagainsthumanity;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class FileIO {
 
-	private File whiteFile = new File("WHITE_CARDS.dat");
-	private File blackFile = new File("BLACK_CARDS.dat");
 	private final int WHITE = 0;
 	private final int BLACK = 1;
-	
-	
-	
-	public ArrayList<Card> createWhiteCards() throws FileNotFoundException {
+
+	public ArrayList<Card> createWhiteCards() throws Exception {
 		ArrayList<Card> whiteCards = new ArrayList<Card>();
-		Scanner whiteScanner = new Scanner(whiteFile);
 		
-		while (whiteScanner.hasNext()) {
-			String line = whiteScanner.nextLine();
-			whiteCards.add(new WhiteCard(line));
+		try {
+			BufferedReader BR = new BufferedReader(new FileReader(new File("WHITE_CARDS.dat")));
+			String line;
+			
+			while ((line = BR.readLine()) != null) {
+				whiteCards.add(new WhiteCard(line));
+			}
+		}
+		catch (Exception e) {
+			
 		}
 		
 		return whiteCards;
@@ -29,11 +33,17 @@ public class FileIO {
 	
 	public ArrayList<Card> createBlackCards() throws FileNotFoundException {
 		ArrayList<Card> blackCards = new ArrayList<Card>();
-		Scanner blackScanner = new Scanner(whiteFile);
-		
-		while (blackScanner.hasNext()) {
-			String line = blackScanner.nextLine();
-			blackCards.add(new BlackCard(line));
+	
+		try {
+			BufferedReader BR = new BufferedReader(new FileReader(new File("BLACK_CARDS.dat")));
+			String line;
+			
+			while ((line = BR.readLine()) != null) {
+				blackCards.add(new BlackCard(line));
+			}
+		}
+		catch (Exception e) {
+			
 		}
 		
 		return blackCards;

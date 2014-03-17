eaee7bf539aa4bc7484f060b7ce5cde168bc2cd1
Andrew Meissner <andrew.meissner@outlook.com>
2014-03-17 13:29:19 -0600

    Continued working on FileIO class
    Moved the card DATS to the assets folder.  Trying to utilize better
    android programming techniques and resources.  Trying a different
    approach to reading in the files as my initial approach went
    invalidated.

--- src/com/csci491/cardsagainsthumanity/FileIO.java
+++ src/com/csci491/cardsagainsthumanity/FileIO.java
@@ -2,28 +2,42 @@ package com.csci491.cardsagainsthumanity;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+
+import android.app.Activity;
 
 /* ========================================================================
  * This is the file input and output class
  * It reads the card file in, as well as the user config file
  * ======================================================================== */
 
-public class FileIO {
+public class FileIO extends Activity {
 	
 	public ArrayList<Card> createWhiteCards() throws Exception {
 		ArrayList<Card> whiteCards = new ArrayList<Card>();
 		
 		try {
-			BufferedReader BR = new BufferedReader(new FileReader(new File("WHITE_CARDS.dat")));
+			/*
+			InputStream is = getAssets().open("WHITE_CARDS.dat");
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			*/
+			
+			//Provided the file is NOT in the assets folder... (original theory)
+			//BufferedReader BR = new BufferedReader(new FileReader(new File("WHITE_CARDS.dat")));
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(getAssets().open("WHITE_CARDS.dat")));
+			
 			String line;
 			
-			while ((line = BR.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				whiteCards.add(new WhiteCard(line));
 			}
+			
+			br.close();
+			
 		}
 		catch (Exception e) {
 			

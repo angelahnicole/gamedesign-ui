6b31b88752d7ac537960bf66b91389732798d5f6
Vinicius Garcia <viniciusgarc@hotmail.com>
2014-03-20 22:50:22 -0600

    Add Start New Game Screen and link to the main screen

--- src/com/csci491/cardsagainsthumanity/MainActivity.java
+++ src/com/csci491/cardsagainsthumanity/MainActivity.java
@@ -9,12 +9,18 @@
 
 package com.csci491.cardsagainsthumanity;
 
+import com.csci491.cardsagainsthumanity.R;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,20 +31,27 @@ public class MainActivity extends Activity {
 			FileIO reader = new FileIO();
 			Globals globals = (Globals) this.getApplicationContext();
 			globals.setWhiteCards(reader.createWhiteCards());
-			//globals.setBlackCards(reader.createBlackCards());
+			// globals.setBlackCards(reader.createBlackCards());
 			System.out.println(globals.getWhiteCards().size());
-			for (int i = 0; i < globals.getWhiteCards().size(); i++)
-			{
+			for (int i = 0; i < globals.getWhiteCards().size(); i++) {
 				System.out.println(globals.getWhiteCards().get(i).getContent());
 			}
 			System.out.println("In the try after everything");
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("Something went wrong somewhere");
 		}
 		System.out.println("after the try / catch");
-		
+
+		Button buttonStart = (Button) findViewById(R.id.buttonStart);
+		buttonStart.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this,
+						StartNewGameActivity.class);
+				startActivity(intent);
+				// finish();
+			}
+		});
+
 	}
 
 	@Override
---
+++ src/com/csci491/cardsagainsthumanity/StartNewGameActivity.java
@@ -0,0 +1,17 @@
+package com.csci491.cardsagainsthumanity;
+
+import com.csci491.cardsagainsthumanity.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class StartNewGameActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_new_game);
+		// rest of the code
+	}
+}
+//

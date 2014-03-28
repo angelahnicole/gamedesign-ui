1164d225114037977fbe73a8e558ba94d47ec66d
Andrew Meissner <andrew.meissner@outlook.com>
2014-03-28 12:16:00 -0600

    moved player class to source package

---
+++ src/com/csci491/cardsagainsthumanity/Player.java
@@ -0,0 +1,70 @@
+package com.csci491.cardsagainsthumanity;
+import java.util.ArrayList;
+
+import android.app.Activity;
+
+
+
+public class Player extends Activity {
+
+	private Globals globals;
+	private ArrayList<WhiteCard> whiteCards;
+	private ArrayList<BlackCard> blackCards;
+	private ArrayList<WhiteCard> myHand;
+	private String name;
+	
+	
+	public Player() {
+		init();
+	}
+	
+	public Player(String name) {
+		init();
+		setName(name);
+	}
+
+	public ArrayList<WhiteCard> getMyHand() {
+		return myHand;
+	}
+
+	public void setMyHand(ArrayList<WhiteCard> myHand) {
+		this.myHand = myHand;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public WhiteCard playWhiteCard() {
+		WhiteCard play = myHand.remove(0);
+		draw();
+		return play;
+	}
+	
+	public void draw() {
+		if (!whiteCards.isEmpty()) {
+			myHand.add(whiteCards.remove(0));
+		}
+		else {
+			shuffleWhiteCards();
+			draw();
+		}
+	}
+	
+	private void shuffleWhiteCards() {
+		FileIO myFileIO = new FileIO();
+		globals.setWhiteCards(myFileIO.hardCodedWhiteCards());
+		whiteCards = globals.getWhiteCards();
+	}
+
+	private void init() {
+		globals = (Globals) this.getApplicationContext();
+		whiteCards = globals.getWhiteCards();
+		blackCards = globals.getBlackCards();
+	}
+	
+}

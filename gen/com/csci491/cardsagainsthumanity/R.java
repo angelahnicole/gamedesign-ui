324f62db6865a17396e123561e0e71fde9351fd4
Vinicius Garcia <viniciusgarc@hotmail.com>
2014-03-29 00:49:10 -0600

    Visual tweaks

--- gen/com/csci491/cardsagainsthumanity/R.java
+++ gen/com/csci491/cardsagainsthumanity/R.java
@@ -10,6 +10,12 @@ package com.csci491.cardsagainsthumanity;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f060001;
+        public static final int blackT=0x7f060003;
+        public static final int white=0x7f060000;
+        public static final int whiteT=0x7f060002;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -21,32 +27,34 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080016;
-        public static final int buttonCancel=0x7f080012;
-        public static final int buttonHow=0x7f08000b;
-        public static final int buttonLeft=0x7f080003;
-        public static final int buttonMiddle=0x7f080004;
-        public static final int buttonRight=0x7f080005;
-        public static final int buttonSkip=0x7f080015;
-        public static final int buttonStart=0x7f080009;
-        public static final int buttonStartGame=0x7f080013;
-        public static final int editText2=0x7f08000d;
-        public static final int editText5=0x7f080010;
-        public static final int progressBar1=0x7f080007;
-        public static final int tableRow1=0x7f080000;
-        public static final int tableRow3=0x7f08000a;
-        public static final int tableRow5=0x7f08000e;
-        public static final int tableRow9=0x7f080006;
-        public static final int textView1=0x7f080008;
-        public static final int textView2=0x7f080014;
-        public static final int textView3=0x7f08000c;
-        public static final int textView3_1=0x7f080002;
-        public static final int textView5=0x7f08000f;
-        public static final int textView5_1=0x7f080011;
-        public static final int textViewQuestion=0x7f080001;
+        public static final int action_settings=0x7f090016;
+        public static final int buttonCancel=0x7f090011;
+        public static final int buttonCard=0x7f090004;
+        public static final int buttonHow=0x7f090008;
+        public static final int buttonLeft=0x7f090003;
+        public static final int buttonRight=0x7f090005;
+        public static final int buttonSkip=0x7f090015;
+        public static final int buttonStart=0x7f090006;
+        public static final int buttonStartGame=0x7f090012;
+        public static final int editText2=0x7f09000b;
+        public static final int editText5=0x7f09000e;
+        public static final int imageView1=0x7f090009;
+        public static final int tableRow1=0x7f090000;
+        public static final int tableRow3=0x7f090007;
+        public static final int tableRow5=0x7f09000c;
+        public static final int tableRow9=0x7f090010;
+        public static final int textView1=0x7f090013;
+        public static final int textView2=0x7f090014;
+        public static final int textView3=0x7f09000a;
+        public static final int textView3_1=0x7f090002;
+        public static final int textView5=0x7f09000d;
+        public static final int textView5_1=0x7f09000f;
+        public static final int textViewQuestion=0x7f090001;
     }
     public static final class layout {
         public static final int activity_ingame=0x7f030000;
@@ -55,7 +63,7 @@ public final class R {
         public static final int activity_start_new_round=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -63,6 +71,8 @@ public final class R {
         public static final int cancel=0x7f050008;
         public static final int hello_world=0x7f050002;
         public static final int how_to_play=0x7f050004;
+        /**  In-game screen 
+         */
         public static final int left=0x7f05000d;
         public static final int max_players=0x7f050006;
         /**  Start New Game screen 
@@ -100,10 +110,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }

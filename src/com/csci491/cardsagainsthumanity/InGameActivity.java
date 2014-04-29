cbe32e705da632343aae4c938fe64992e4295b52
Vinicius Garcia <viniciusgarc@hotmail.com>
2014-04-29 15:45:33 -0600

    scroll through cards: continues scrolling even if you get to the last one
    if u reach last card, goes back to first one.
    if u are going back and reach the first card, goes to the last one

--- src/com/csci491/cardsagainsthumanity/InGameActivity.java
+++ src/com/csci491/cardsagainsthumanity/InGameActivity.java
@@ -1,6 +1,5 @@
 package com.csci491.cardsagainsthumanity;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -14,73 +13,93 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class InGameActivity extends Activity {
-//	@SuppressLint("ResourceAsColor")
+	// @SuppressLint("ResourceAsColor")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ingame);
 
-		System.out.println("InGameActivity: WhiteCards: " + Globals.getWhiteCards().size());
-		System.out.println("InGameActivity: BlackCards: " + Globals.getBlackCards().size());
+		System.out.println("InGameActivity: WhiteCards: "
+				+ Globals.getWhiteCards().size());
+		System.out.println("InGameActivity: BlackCards: "
+				+ Globals.getBlackCards().size());
 
-		// If this screen is displayed again for another player but in the same round the question
-		// must not change. Change only if it's another round. Property "NewBlackCard" defines
+		// If this screen is displayed again for another player but in the same
+		// round the question
+		// must not change. Change only if it's another round. Property
+		// "NewBlackCard" defines
 		// whether it's a new round or not.
 		if (Globals.changeBlackCard()) {
 			Globals.getBlackCards().remove(0);
 			Globals.setChangeBlackCard(false);
 		}
-		
+
 		// Set question (Black card)
 		TextView question = (TextView) findViewById(R.id.textViewQuestion);
 		question.setText(Globals.getBlackCards().get(0).getContent());
-		
+
 		// Set white cards based on player's hand
 		Button buttonCard = (Button) findViewById(R.id.buttonCard);
-		buttonCard.setText(Globals.getPlayers().get(Globals.getIndexHumanPlayer()).getMyHand().get(0).getContent());
+		buttonCard.setText(Globals.getPlayers()
+				.get(Globals.getIndexHumanPlayer()).getMyHand().get(0)
+				.getContent());
 		buttonCard.setOnClickListener(cardListener);
 
 		// navigation (white cards)
 		Globals.setIndexWhiteCard(0);
 		Button buttonLeft = (Button) findViewById(R.id.buttonLeft);
 		Button buttonRight = (Button) findViewById(R.id.buttonRight);
-		
+
 		// Set listeners on navigation
 		buttonLeft.setOnClickListener(leftListener);
 		buttonRight.setOnClickListener(rightListener);
 
 	}
-	
+
 	// ========================================================================
-	//                              BUTTON LISTENERS
+	// BUTTON LISTENERS
 	// ========================================================================
-	
+
 	private OnClickListener leftListener = new OnClickListener() {
 		public void onClick(View v) {
+			Button card = (Button) findViewById(R.id.buttonCard);
+
+			Button submit = (Button) findViewById(R.id.buttonSubmit);
+			submit.setVisibility(View.GONE);
+
 			// verify if it's possible to go back
 			if (Globals.getIndexWhiteCard() > 0) {
+				//yes, so go back
 				Globals.setIndexWhiteCard(Globals.getIndexWhiteCard() - 1);
-				Button card = (Button) findViewById(R.id.buttonCard);
-				card.setText(Globals.getPlayers().get(Globals.getIndexHumanPlayer()).getMyHand().get(Globals.getIndexWhiteCard()).getContent());
-
-				Button submit = (Button) findViewById(R.id.buttonSubmit);
-				submit.setVisibility(View.GONE);
+			} else {
+				//no, so go to last card
+				Globals.setIndexWhiteCard(6);
 			}
+			card.setText(Globals.getPlayers()
+					.get(Globals.getIndexHumanPlayer()).getMyHand()
+					.get(Globals.getIndexWhiteCard()).getContent());
+
 		}
 	};
-	
+
 	private OnClickListener rightListener = new OnClickListener() {
 		public void onClick(View v) {
+			Button card = (Button) findViewById(R.id.buttonCard);
+			Button submit = (Button) findViewById(R.id.buttonSubmit);
+			submit.setVisibility(View.GONE);
+
 			// verify if it's possible to go further
 			if (Globals.getIndexWhiteCard() < 6) {
-				Button card = (Button) findViewById(R.id.buttonCard);
+				// yes, so go further
 				Globals.setIndexWhiteCard(Globals.getIndexWhiteCard() + 1);
-				card.setText(Globals.getPlayers().get(Globals.getIndexHumanPlayer()).getMyHand().get(Globals.getIndexWhiteCard()).getContent());
-
-				Button submit = (Button) findViewById(R.id.buttonSubmit);
-				submit.setVisibility(View.GONE);
+			} else {
+				// no, so go back to fist card
+				Globals.setIndexWhiteCard(0);
 			}
+			card.setText(Globals.getPlayers()
+					.get(Globals.getIndexHumanPlayer()).getMyHand()
+					.get(Globals.getIndexWhiteCard()).getContent());
 		}
 	};
 
@@ -94,44 +113,58 @@ public class InGameActivity extends Activity {
 			} else {
 				buttonSubmit.setVisibility(View.GONE);
 			}
-			
+
 			// Call the listener of the button
 			buttonSubmit.setOnClickListener(submitListener);
 		}
 	};
-	
+
 	private OnClickListener submitListener = new OnClickListener() {
 		public void onClick(View arg0) {
-			//stores who submitted this card in the property "owner"
-			Globals.getPlayers().get(Globals.getIndexHumanPlayer()).getMyHand().get(Globals.getIndexWhiteCard()).setOwner(Globals.getPlayers().get(Globals.getIndexHumanPlayer()));
+			// stores who submitted this card in the property "owner"
+			Globals.getPlayers()
+					.get(Globals.getIndexHumanPlayer())
+					.getMyHand()
+					.get(Globals.getIndexWhiteCard())
+					.setOwner(
+							Globals.getPlayers().get(
+									Globals.getIndexHumanPlayer()));
 
 			/*
-			//Add current white card (actual card in had) to the list of cards played this round (plays)
-			Globals.getPlays().add(Globals.getPlayers().get(Globals.getIndexHumanPlayer()).getMyHand().get(Globals.getIndexWhiteCard()));
-			
-			//Remove the card just played from player's hand
-			Globals.getPlayers().get(Globals.getIndexHumanPlayer()).getMyHand().remove(0);
-			
-			//Add a new card in order to substitute the one that was just removed
-			//NOT SURE IF WE HAVE TO IMPLEMENT THIS!
-			*/
-			
-			// Calling this method will remove the card from the players hand and make the player draw a new card from the white pile
-			// additionally, if the white pile is empty, the player will re-shuffle the white deck and draw from it
-			Globals.getPlays().add(Globals.getPlayers().get(Globals.getIndexHumanPlayer()).playWhiteCard(Globals.getIndexWhiteCard()));
-			
-			//Redirect to Player Turn screen
+			 * //Add current white card (actual card in had) to the list of
+			 * cards played this round (plays)
+			 * Globals.getPlays().add(Globals.getPlayers
+			 * ().get(Globals.getIndexHumanPlayer
+			 * ()).getMyHand().get(Globals.getIndexWhiteCard()));
+			 * 
+			 * //Remove the card just played from player's hand
+			 * Globals.getPlayers
+			 * ().get(Globals.getIndexHumanPlayer()).getMyHand().remove(0);
+			 * 
+			 * //Add a new card in order to substitute the one that was just
+			 * removed //NOT SURE IF WE HAVE TO IMPLEMENT THIS!
+			 */
+
+			// Calling this method will remove the card from the players hand
+			// and make the player draw a new card from the white pile
+			// additionally, if the white pile is empty, the player will
+			// re-shuffle the white deck and draw from it
+			Globals.getPlays().add(
+					Globals.getPlayers().get(Globals.getIndexHumanPlayer())
+							.playWhiteCard(Globals.getIndexWhiteCard()));
+
+			// Redirect to Player Turn screen
 			Intent intent = new Intent(InGameActivity.this,
 					PlayerTurnActivity.class);
 			startActivity(intent);
 			finish();
 		}
 	};
-	
+
 	// ========================================================================
-	//                                OPTIONS
+	// OPTIONS
 	// ========================================================================
-	
+
 	// Show menu
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -155,7 +188,8 @@ public class InGameActivity extends Activity {
 					new DialogInterface.OnClickListener() {
 						public void onClick(DialogInterface dialog, int id) {
 							// Leave Game
-							Intent intent = new Intent(InGameActivity.this, MainActivity.class);
+							Intent intent = new Intent(InGameActivity.this,
+									MainActivity.class);
 							startActivity(intent);
 						}
 					});
